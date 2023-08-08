3763e21de1e15d627df2af8c97eb91fc86c93701
Vanessa Lacuesta <vane.lacuesta.3@gmail.com>
2023-08-08 19:19:30 -0300

    correcciones

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const productoTodos = [];
 
 const producto0 = new producto (0, "Destornillador" , 50);
 const producto1 = new producto (1, "Taladro" , 2000);
-const producto2 = new producto (2, "Clavo" , 2);
-const producto3 = new producto (3, "Pinza" , 50);
-const producto4 = new producto (4, "Martillo" , 50);
+const producto2 = new producto (2, "Clavo" , 1);
+const producto3 = new producto (3, "Tornillo" , 2);
+const producto4 = new producto (4, "Martillo" , 250);
 
 productoTodos.push(producto0, producto1, producto2, producto3, producto4);
 
@@ -21,7 +21,7 @@ const buscarProducto = prompt("Bienvenido a la ferretería! Tenemos Destornillad
 const identificarProducto = productoTodos.find ( (producto)=> producto.nombre.toLowerCase() == buscarProducto.toLowerCase());
 
 if (identificarProducto){
-  alert( `El ${identificarProducto.nombre} está disponible por ${identificarProducto.precio}`);
+  alert( `El ${identificarProducto.nombre} está disponible por $ ${identificarProducto.precio}`);
 } else {
   alert("Lo sentimos, el producto que busca no está disponible.");
 }
