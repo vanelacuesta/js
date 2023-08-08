3e4b65223c4e0c48b9bf170171abd9328c6dd03c
Vanessa Lacuesta <vane.lacuesta.3@gmail.com>
2023-08-08 19:30:16 -0300

    correcciones

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 class producto {
-  constructor(id, nombre, precio) {
+  constructor(id, nombre, precio, stock) {
     this.id = id;
     this.nombre = nombre;
     this.precio = precio;
+    this.stock = stock;
   }
 }
 
 const productoTodos = [];
 
-const producto0 = new producto (0, "Destornillador" , 50);
-const producto1 = new producto (1, "Taladro" , 2000);
-const producto2 = new producto (2, "Clavo" , 1);
-const producto3 = new producto (3, "Tornillo" , 2);
-const producto4 = new producto (4, "Martillo" , 250);
+const producto0 = new producto (0, "Destornillador" , 50, 8);
+const producto1 = new producto (1, "Taladro" , 2000, 10);
+const producto2 = new producto (2, "Clavo" , 1, 60);
+const producto3 = new producto (3, "Tornillo" , 2, 50);
+const producto4 = new producto (4, "Martillo" , 250, 5);
 
 productoTodos.push(producto0, producto1, producto2, producto3, producto4);
 
@@ -21,7 +22,7 @@ const buscarProducto = prompt("Bienvenido a la ferretería! Tenemos Destornillad
 const identificarProducto = productoTodos.find ( (producto)=> producto.nombre.toLowerCase() == buscarProducto.toLowerCase());
 
 if (identificarProducto){
-  alert( `El ${identificarProducto.nombre} está disponible por $ ${identificarProducto.precio}`);
+  alert( `Tenemos ${identificarProducto.nombre}s disponibles y su precio es $ ${identificarProducto.precio} apurate que sólo nos quedan ${identificarProducto.stock}!`);
 } else {
   alert("Lo sentimos, el producto que busca no está disponible.");
 }
