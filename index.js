b51ee3e2288988a4a07c82cf0735552333ce17a8
Vanessa Lacuesta <vane.lacuesta.3@gmail.com>
2023-08-22 23:20:23 -0300

    correcciones

--- index.js
+++ index.js
@@ -41,9 +41,42 @@ const cardsAHtml = array => {
         <h2>
         ${element.name} ${element.marca}
         </h2>
+        <button class="button-card" id="button-${element.id}">
+          <h3> ¡Agregar al carrito!</h3>
+        </button> 
       </div>
   `
   }, "")
   productosContainer.innerHTML = cards
 }
 cardsAHtml(productos)
+
+const todosLosProductos = document.querySelectorAll(".button-card")
+
+let agregadosAlCarrito=[]
+
+const eventoCards = ( nodos, array ) => {
+
+     for ( let i = 0; i < nodos.length; i++ ) {
+
+         nodos[i].onclick = (e) => {                               
+             const id = e.currentTarget.id.slice(7)
+             const buscarProducto = array.find( element => element.id === Number(id))
+             agregadosAlCarrito.push(buscarProducto)
+             localStorage.setItem("productos", JSON.stringify(agregadosAlCarrito))
+             Toastify({
+                 text: `¡Se ha añadido  ${ buscarProducto.name } a tu carrito!`,
+                 duration: 3000,
+                 gravity: "botton",
+                 className: "info",
+                 style: {
+                     background: "linear-gradient(to right, black, gray)",
+                 }
+
+             }).showToast();
+        }
+    }
+}
+
+eventoCards(todosLosProductos, productos)
+console.log (agregadosAlCarrito)
