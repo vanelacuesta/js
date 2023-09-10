53178781ba6c41d6f78a8ad3fd21e5a3e6b6f34d
Vanessa Lacuesta <vane.lacuesta.3@gmail.com>
2023-09-10 16:22:53 -0300

    cambios

--- index.js
+++ index.js
@@ -1,55 +1,84 @@
+const url = `https://dummyjson.com/products`;
 
-console.log(productos)
-const productosContainer = document.querySelector("#productos-container")
-
-const cardsAHtml = array => {
-
-  const cards = array.reduce((acc, element) => {
-      return acc + `
-      <div class="card" id="card-${element.id}">
-        <figure>
-          <img src=${element.imagen|| "./img/not-found.jpg"} alt="${element.name}">
-        </figure>
-        <h2>
-        ${element.name} ${element.marca}
-        </h2>
-        <button class="button-card" id="button-${element.id}">
-          <h3> ¡Agregar al carrito!</h3>
-        </button> 
-      </div>
-  `
-  }, "")
-  productosContainer.innerHTML = cards
-}
-cardsAHtml(productos)
-
-const todosLosProductos = document.querySelectorAll(".button-card")
-
-let agregadosAlCarrito=[]
-
-const eventoCards = ( nodos, array ) => {
-
-     for ( let i = 0; i < nodos.length; i++ ) {
-
-         nodos[i].onclick = (e) => {                               
-             const id = e.currentTarget.id.slice(7)
-             const buscarProducto = array.find( element => element.id === Number(id))
-             agregadosAlCarrito.push(buscarProducto)
-             localStorage.setItem("productos", JSON.stringify(agregadosAlCarrito))
-             Toastify({
-                 text: `¡Se ha añadido  ${ buscarProducto.name} ${buscarProducto.marca } a tu carrito!`,
-                 duration: 3000,
-                 gravity: "bottom",
-                 close: true,
-                 style: {
-                     background: "linear-gradient(to right, black, gray)",
-                     border: "solid 1px red",
-                 }
-
-             }).showToast();
-        }
+const cardsAHtml = data => {
+    if (!Array.isArray(data)) {
+        console.error("Error: Data is not an array:", data);
+        return;
     }
-}
 
-eventoCards(todosLosProductos, productos)
+    const cards = data.reduce((acc, elemento) => {
+        return acc + `
+            <div id="producto-${elemento.id}" class="card">
+                <div class="container-img">
+                    <img src=${elemento.images[0] || "./img/not-found.jpg" } alt=${elemento.title}>
+                </div> 
+                <h2>
+                    ${elemento.title}
+                </h2>
+                <button class="button-card" id="button-${elemento.id}">
+                  <h3> ¡Agregar al carrito!</h3>
+                </button> 
+            </div>
+        `;
+    }, "");
+    document.querySelector("#productos-container").innerHTML = cards;
+
+    return document.querySelectorAll(".button-card");
+};
+
+const changeImageOnHover = (data) => {
+    const cards = document.querySelectorAll('.card');
+    
+    cards.forEach((card, index) => {
+        const img = card.querySelector('img');
+        
+        const originalImage = img.src;
+        const hoverImage = data[index].images[1] || data[index].images[0]; 
+        
+        card.addEventListener('mouseenter', () => {
+            img.src = hoverImage;
+        });
+
+        card.addEventListener('mouseleave', () => {
+            img.src = originalImage;
+        });
+    });
+};
+
+const eventoCards = (nodos, array) => {
+    nodos.forEach(nodo => {
+        nodo.onclick = (e) => {
+            const id = e.currentTarget.id.slice(7);
+            const buscarProducto = array.find(element => element.id === Number(id));
+            agregadosAlCarrito.push(buscarProducto);
+            localStorage.setItem("productos", JSON.stringify(agregadosAlCarrito));
+            Toastify({
+                text: `¡Se ha añadido ${buscarProducto.title} a tu carrito!`,
+                duration: 3000,
+                gravity: "bottom",
+                close: true,
+                style: {
+                    background: "linear-gradient(to right, black, gray)",
+                    border: "solid 1px red",
+                }
+            }).showToast();
+        };
+    });
+};
+
+let agregadosAlCarrito = [];
+
+
+
+fetch(url)
+    .then(res => res.json())
+    .then(json => {
+        console.log(json); 
+        const botones = cardsAHtml(json.products);
+        changeImageOnHover(json.products);
+        eventoCards(botones, json.products);
+    })
+    .catch(error => {
+        console.error("Hubo un error al obtener los datos:", error);
+    });
 console.log (agregadosAlCarrito)
