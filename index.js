56c3c0990c8660d615e58bf7af2ab6a779fb9057
Vanessa Lacuesta <vane.lacuesta.3@gmail.com>
2023-09-10 20:48:19 -0300

    cambios

--- index.js
+++ index.js
@@ -1,47 +1,52 @@
 const url = `https://dummyjson.com/products`;
+let agregadosAlCarrito = [];
+
+const updateCart = () => {
+    const listaCarrito = document.querySelector("#lista-carrito");
+    listaCarrito.innerHTML = "";
+    
+    let total = 0;
+
+    agregadosAlCarrito.forEach(producto => {
+        const subtotal = producto.price * producto.cantidad;
+        const li = document.createElement("li");
+        li.textContent = `${producto.title} - $${producto.price} x ${producto.cantidad} = $${subtotal.toFixed(2)}`; 
+        listaCarrito.appendChild(li);
+        total += subtotal;
+    });
+
+    const totalElement = document.createElement("li");
+    totalElement.style.fontWeight = "bold";
+    totalElement.textContent = `Total: $${total.toFixed(2)}`;
+    listaCarrito.appendChild(totalElement);
+};
 
 const cardsAHtml = data => {
-    if (!Array.isArray(data)) {
-        console.error("Error: Data is not an array:", data);
-        return;
-    }
-
-    const cards = data.reduce((acc, elemento) => {
-        return acc + `
+    const cards = data.reduce((acc, elemento) => acc + `
             <div id="producto-${elemento.id}" class="card">
                 <div class="container-img">
                     <img src=${elemento.images[0] || "./img/not-found.jpg" } alt=${elemento.title}>
-                </div> 
-                <h2>
-                    ${elemento.title}
-                </h2>
+                </div>
+                <h2>${elemento.title}</h2>
+                <div class="precio">$${elemento.price}</div> <!-- Aquí se muestra el precio -->
                 <button class="button-card" id="button-${elemento.id}">
-                  <h3> ¡Agregar al carrito!</h3>
-                </button> 
+                    <i class="fas fa-shopping-cart button-card" id="button-${elemento.id}"></i>
+                </button>
             </div>
-        `;
-    }, "");
-    document.querySelector("#productos-container").innerHTML = cards;
+        `, "");
 
+    document.querySelector("#productos-container").innerHTML = cards;
     return document.querySelectorAll(".button-card");
 };
 
 const changeImageOnHover = (data) => {
     const cards = document.querySelectorAll('.card');
-    
     cards.forEach((card, index) => {
         const img = card.querySelector('img');
-        
         const originalImage = img.src;
-        const hoverImage = data[index].images[1] || data[index].images[0]; 
-        
-        card.addEventListener('mouseenter', () => {
-            img.src = hoverImage;
-        });
-
-        card.addEventListener('mouseleave', () => {
-            img.src = originalImage;
-        });
+        const hoverImage = data[index].images[1] || data[index].images[0];
+        card.addEventListener('mouseenter', () => img.src = hoverImage);
+        card.addEventListener('mouseleave', () => img.src = originalImage);
     });
 };
 
@@ -50,8 +55,17 @@ const eventoCards = (nodos, array) => {
         nodo.onclick = (e) => {
             const id = e.currentTarget.id.slice(7);
             const buscarProducto = array.find(element => element.id === Number(id));
-            agregadosAlCarrito.push(buscarProducto);
+            const productoEnCarrito = agregadosAlCarrito.find(p => p.id === buscarProducto.id);
+
+            if (productoEnCarrito) {
+                productoEnCarrito.cantidad += 1;
+            } else {
+                buscarProducto.cantidad = 1;
+                agregadosAlCarrito.push(buscarProducto);
+            }
+
             localStorage.setItem("productos", JSON.stringify(agregadosAlCarrito));
+            updateCart();
             Toastify({
                 text: `¡Se ha añadido ${buscarProducto.title} a tu carrito!`,
                 duration: 3000,
@@ -66,19 +80,51 @@ const eventoCards = (nodos, array) => {
     });
 };
 
-let agregadosAlCarrito = [];
-
-
+function searchProducts(term) {
+    fetch(url)
+        .then(res => res.json())
+        .then(json => {
+            const filteredProducts = json.products.filter(product => product.title.toLowerCase().includes(term) || product.description.toLowerCase().includes(term));
+            if (filteredProducts.length === 0) {
+                document.querySelector("#productos-container").innerHTML = '<p>No hay ningún resultado para tu búsqueda. Intenta buscar de otra manera!</p>';
+            } else {
+                const botones = cardsAHtml(filteredProducts);
+                changeImageOnHover(filteredProducts);
+                eventoCards(botones, filteredProducts);
+            }
+        })
+        .catch(error => console.error("Hubo un error al obtener los datos:", error));
+}
 
 fetch(url)
     .then(res => res.json())
     .then(json => {
-        console.log(json); 
         const botones = cardsAHtml(json.products);
         changeImageOnHover(json.products);
         eventoCards(botones, json.products);
     })
-    .catch(error => {
-        console.error("Hubo un error al obtener los datos:", error);
+    .catch(error => console.error("Hubo un error al obtener los datos:", error));
+
+document.querySelector("form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    const searchTerm = document.getElementById("inputData").value.toLowerCase();
+    searchProducts(searchTerm);
+});
+
+if(localStorage.getItem("productos")) {
+    agregadosAlCarrito = JSON.parse(localStorage.getItem("productos"));
+    agregadosAlCarrito.forEach(producto => {
+        if(!producto.cantidad) {
+            producto.cantidad = 1;
+        }
     });
-console.log (agregadosAlCarrito)
+    updateCart();
+}
+function vaciarCarrito() {
+    agregadosAlCarrito = []; 
+    localStorage.removeItem("productos"); 
+    updateCart(); 
+}
+document.getElementById("vaciar-carrito").addEventListener("click", vaciarCarrito);
+console.log(agregadosAlCarrito)
+console.log(cardsAHtml)
